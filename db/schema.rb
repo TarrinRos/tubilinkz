1691816b4394577c6d063ab0a612df603520872a
TarrinRos <tarindis@gmail.com>
2019-02-12 18:24:07 +0200

    Add name field for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206134640) do
+ActiveRecord::Schema.define(version: 20190212160359) do
 
   create_table "links", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20190206134640) do
     t.boolean "admin"
     t.string "provider"
     t.string "url"
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
